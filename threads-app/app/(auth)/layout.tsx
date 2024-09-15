cec1a83362699ff07453ef2e6f7d2d06f3378576
Nguyen Huy Vu <vunhhe161239@fpt.edu.vn>
2024-09-15 20:20:50 +0700

    b1

---
+++ threads-app/app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+import "../global.css";
+
+export const metadata = {
+  title: "Threads",
+  description: "Threads app",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
