fd5e73ad51eb7d78f493102a586918adac699b63
Nguyen Huy Vu <vunhhe161239@fpt.edu.vn>
2024-09-17 15:13:24 +0700

    b3

--- threads-app/app/(root)/page.tsx
+++ threads-app/app/(root)/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <>
-      <h1  className="head-text text-left"> Home</h1>
-    </>
+    <div className="mt-9 flex flex-col gap-9">
+      <section className="flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1"> Trending </h1>
+
+        <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
+          {/* {tasks?.map(({ _id }) => <div key={_id}>text</div>)} */}
+        </div>
+
+        <div className="podcast_grid"></div>
+      </section>
+    </div>
   );
 }
